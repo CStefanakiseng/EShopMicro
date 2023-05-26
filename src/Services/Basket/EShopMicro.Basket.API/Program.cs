a3558a0a5531b52e43503c5739a1056e83d7f69a
c.stefanakis <c.stefanakis@dataverse.gr>
2023-05-26 13:41:33 +0300

    basket microservice crud with redis

--- src/Services/Basket/EShopMicro.Basket.API/Program.cs
+++ src/Services/Basket/EShopMicro.Basket.API/Program.cs
@@ -1,11 +1,25 @@
+using EShopMicro.Basket.API.Repositories;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
+});
+
+
 
 var app = builder.Build();
 
@@ -13,7 +27,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
 }
 
 app.UseAuthorization();
---
+++ src/Services/Basket/EShopMicro.Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using EShopMicro.Basket.API.Entities;
+
+namespace EShopMicro.Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
