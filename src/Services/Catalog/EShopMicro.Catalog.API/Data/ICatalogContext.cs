f778080cf2c33061a9fa64ae31b6eacb4a2e2c7d
c.stefanakis <c.stefanakis@dataverse.gr>
2023-05-26 09:20:16 +0300

    Catalog Microservice

---
+++ src/Services/Catalog/EShopMicro.Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using EShopMicro.Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace EShopMicro.Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
