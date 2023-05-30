4de60dc09c94c292870882f72799fc51734ec339
c.stefanakis <c.stefanakis@dataverse.gr>
2023-05-30 15:57:22 +0300

    Discount Grpc Service

---
+++ src/Services/Discount/EShopMicro.Discount.Grpc/Profiles/DiscountProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EShopMicro.Discount.Grpc.Entities;
+using EShopMicro.Discount.Grpc.Protos;
+
+namespace EShopMicro.Discount.Grpc.Profiles
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
